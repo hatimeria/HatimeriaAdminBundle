d10ce5cd2b611069d70e59b91b2f21fb454c9659
Zbigniew Łabacz <zbigniew.labacz@hatimeria.pl>
2011-10-11 18:31:59 +0200

    locale, fix in categoriestree

--- Resources/public/js/tree/CategoriesTree.js
+++ Resources/public/js/tree/CategoriesTree.js
@@ -56,7 +56,7 @@
             
             selNode.expand();
             
-            Ext.Msg.prompt(Ext.String.format('Nowa kategoria w: {0}', selNode.get('text')), 'Poda nazwę kategorii', function(btn, value) {
+            Ext.Msg.prompt(Ext.String.format('Nowa kategoria w: {0}', selNode.get('text')), 'Podaj nazwę kategorii', function(btn, value) {
                 _this.addCategory(value, selNode);
             });
         },
