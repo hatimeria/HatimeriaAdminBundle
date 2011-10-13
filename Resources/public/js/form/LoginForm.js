62406888c5143f82ee76953667d454ae52d39f61
Zbigniew Łabacz <zbigniew.labacz@hatimeria.pl>
2011-10-13 12:37:54 +0200

    refactoring login form

--- Resources/public/js/form/LoginForm.js
+++ Resources/public/js/form/LoginForm.js
@@ -1,36 +1,51 @@
 Ext.define("HatimeriaAdmin.form.LoginForm", {
     extend: "HatimeriaCore.form.BaseForm",
     transNS: "form.login",
-    layout: 'anchor',
-    bodyPadding: 10,
-    defaults: {
-        anchor: '95%'
-    },
-    defaultType: 'textfield',
-    constructor: function(cfg) {
-        
+
+    /**
+     * Constructor
+     * 
+     * @param {} cfg
+     */
+    constructor: function(cfg)
+    {
         var config = cfg || {};
         config.url = Routing.generate('fos_user_security_check');
         
         this.callParent([config]);
     },
-    initComponent: function() {
+    
+    /**
+     * Initialize component
+     */
+    initComponent: function()
+    {
         this.submitConfig = {
             text: this.__('submit'),
             success: function() { window.location = Routing.generate('homepage') }
         };
         
-        this.items = [{
-                fieldLabel: this.__('login'),
-                name: '_username',
-                allowBlank: false
-            },{
-                fieldLabel: this.__('password'),
-                inputType: 'password',
-                name: '_password',
-                allowBlank: false
-            }];
-
+        var config = {
+            layout: 'auto',
+            bodyPadding: 10,
+            border: 0,
+            defaultType: 'textfield',
+            items: [
+                {
+                    fieldLabel: this.__('login'),
+                    name: '_username',
+                    allowBlank: false
+                },
+                {
+                    fieldLabel: this.__('password'),
+                    inputType: 'password',
+                    name: '_password',
+                    allowBlank: false
+                }
+            ]
+        };
+        Ext.apply(this, Ext.apply(config, this.initialConfig))
+        
         this.callParent();
     }
 })
