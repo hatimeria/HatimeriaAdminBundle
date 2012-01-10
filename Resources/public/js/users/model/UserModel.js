531ed1b57d95bd133ad6d765f0a674fae4d7be61
Zbigniew Łabacz <zbigniew.labacz@hatimeria.pl>
2012-01-10 13:47:17 +0100

    fix in DirectModel

--- Resources/public/js/users/model/UserModel.js
+++ Resources/public/js/users/model/UserModel.js
@@ -4,10 +4,7 @@
 (function() {
     
     Ext.define('HatimeriaAdmin.users.model.UserModel', {
-        extend: 'Ext.data.Model',
-        mixins: {
-            configurable: 'Hatimeria.core.mixins.ConfigurableExternal'
-        },
+        extend: 'Hatimeria.core.model.DirectModel',
         fields: [
             {name: 'id', type: 'int'},
             {name: 'username', type: 'string'},
