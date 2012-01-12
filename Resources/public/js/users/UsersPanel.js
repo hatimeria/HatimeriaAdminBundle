10d54de2f8ea061d7c651038ba73d92d52810ebf
Zbigniew Łabacz <zbigniew.labacz@hatimeria.pl>
2012-01-12 17:04:51 +0100

    refactoring UsersPanel

--- Resources/public/js/users/UsersPanel.js
+++ Resources/public/js/users/UsersPanel.js
@@ -11,6 +11,7 @@
             var store = Ext.create('HatimeriaAdmin.users.store.UserStore');
             var config = {
                 title: this.__('title'),
+                rowActions: ['edit', 'remove'],
                 id: 'all-users-grid',
                 store: store,
                 columns: [
@@ -23,67 +24,6 @@
             
             Ext.apply(this, this.applyExternal(Ext.apply(config, this.initialConfig)));
             this.callParent();
-        },
-        
-        /**
-         * Items for context menu
-         * 
-         * @return []
-         */
-        getContextMenuItems: function(record, index)
-        {
-            return [
-                {
-                    text: 'Edytuj',
-                    cls: 'ux-edit',
-                    scope: this,
-                    handler: function() {
-                        this.onEditClick(record, index);
-                    }
-                },
-                {
-                    text: 'Usuń',
-                    cls: 'ux-remove',
-                    scope: this,
-                    handler: function() {
-                        this.onRemoveClick(record, index);
-                    }
-                }
-            ];
-        },
-        
-        /**
-         * Event: edit click
-         * 
-         * @param Ext.data.Model record
-         * @param int index
-         */
-        onEditClick: function(record)
-        {   
-            var userwindow = Ext.create('HatimeriaAdmin.users.window.UserWindow', {
-                title: Ext.String.format('Edycja użytkownika: "{0}"', record.get('email'))
-            });
-            userwindow.show();
-            userwindow.populate(record);
-        },
-        
-        /**
-         * Event: edit click
-         * 
-         * @param Ext.data.Model record
-         * @param int index
-         */
-        onRemoveClick: function(record, index)
-        {
-            var store = this.store;
-            
-            Ext.create('Hatimeria.core.response.DirectHandler', {
-                fn: Actions.HatimeriaAdmin_User.remove,
-                params: {id: record.get('id')},
-                success: function() {
-                    store.remove(record);
-                }
-            });               
         }
     });
     
