f49510a190eba2798802bc583296153bd8f5bdd8
Michał Wujas <michal@wujas.pl>
2012-01-12 16:22:52 +0100

    added translations for user grid

--- Resources/public/js/users/UsersPanel.js
+++ Resources/public/js/users/UsersPanel.js
@@ -3,19 +3,21 @@
     Ext.define('HatimeriaAdmin.users.UsersPanel', {   
         extend: 'Hatimeria.core.grid.BaseGrid',
         requires: ["HatimeriaAdmin.users.store.UserStore"],
-
+        transDomain: 'HatimeriaAdminBundle',
+        transNS: 'users',
+        
         initComponent: function()
         {
             var store = Ext.create('HatimeriaAdmin.users.store.UserStore');
             var config = {
-                title: __('users'),
+                title: this.__('title'),
                 id: 'all-users-grid',
                 store: store,
                 columns: [
-                    {header: 'Id', dataIndex: 'id', width: 80},
-                    {header: 'Nazwa', dataIndex: 'username', width: 180},
-                    {header: 'Email', dataIndex: 'email', width: 220},
-                    {header: 'Włączony', dataIndex: 'enabled', width: 80, renderer: this.rendererYesNo}
+                    {dataIndex: 'id', width: 80},
+                    {dataIndex: 'username', width: 180},
+                    {dataIndex: 'email', width: 220},
+                    {dataIndex: 'enabled', width: 80, renderer: this.rendererYesNo}
                 ]
             };
             
